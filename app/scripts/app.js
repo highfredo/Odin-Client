4c077c679159c568ef0c82e8bdf123232e4f782c
Highfredo <highfredo@gmail.com>
2014-10-12 12:45:06 +0200

    muchos cambios

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,4 +8,35 @@
  *
  * Main module of the application.
  */
-var app = angular.module('odinClientApp', ['ui.router', 'ui.bootstrap', 'mainModule']);
+
+var $backendUrl = "http://localhost:8080/"
+	
+var app = angular.module('odinClientApp', ['ngResource', 'ui.router', 'ui.bootstrap', 'listDocumentsModule']);
+
+app.config(function($stateProvider, $urlRouterProvider){
+    $urlRouterProvider.otherwise('/list/');
+    
+    $stateProvider
+        .state('list', {
+            url: '/list/{path:.*}',
+            templateUrl: 'views/list_documents.html',
+            controller: 'listDocumentsCtrl',
+            data: {
+            	ncyBreadcrumbLabel: 'Home' 
+            }
+        })
+        /*.state('about', {
+             
+        });*/
+})
+
+app.factory('BackendUrl', function() {
+	return {
+		get: function() {return $backendUrl}
+	}
+})
+
+app.run(function($rootScope) {
+    $rootScope.backendUrl = $backendUrl;
+})
+
