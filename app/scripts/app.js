b154b4a0d735bd3da8ec0afde200eb1e14b141b9
Highfredo <highfredo@gmail.com>
2014-10-12 07:54:59 +0200

    First commit, template

---
+++ app/scripts/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name odinClientApp
+ * @description
+ * # odinClientApp
+ *
+ * Main module of the application.
+ */
+var app = angular.module('odinClientApp', ['ui.router', 'ui.bootstrap', 'mainModule']);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+
+
+var module = angular.module('mainModule', [])
+ /* .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });*/
