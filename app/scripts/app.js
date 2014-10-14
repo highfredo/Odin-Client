1882ea3a507cafd0aeee02b6b527ecd0c6c36e00
Highfredo <highfredo@gmail.com>
2014-10-14 05:07:27 +0200

    sync with odin server before create maven proyect

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,25 +9,31 @@
  * Main module of the application.
  */
 
-var $backendUrl = "http://localhost:8080/"
+var $backendUrl = "/"; // "http://localhost:8080/"
 	
-var app = angular.module('odinClientApp', ['ngResource', 'ui.router', 'ui.bootstrap', 'listDocumentsModule']);
+var app = angular.module('odinClientApp', ['ngResource', 'ui.router', 'ui.bootstrap', 'angularFileUpload', 'angular-loading-bar', 'listDocumentsModule', 'errorsModule']);
 
-app.config(function($stateProvider, $urlRouterProvider){
+app.config(function($stateProvider, $urlRouterProvider, $httpProvider, cfpLoadingBarProvider){
+	cfpLoadingBarProvider.includeSpinner = false;
+	
     $urlRouterProvider.otherwise('/list/');
     
     $stateProvider
         .state('list', {
             url: '/list/{path:.*}',
             templateUrl: 'views/list_documents.html',
-            controller: 'listDocumentsCtrl',
-            data: {
-            	ncyBreadcrumbLabel: 'Home' 
-            }
+            controller: 'listDocumentsCtrl'
         })
-        /*.state('about', {
-             
-        });*/
+        .state('error', {
+            url: '/error/{code}',
+            templateUrl: 'views/error.html',
+            controller: 'ErrorCtrl',
+            resolve: {
+                errorInfo: function () {
+                    return this.self.error;
+                }
+            }
+        })      
 })
 
 app.factory('BackendUrl', function() {
@@ -36,7 +42,12 @@ app.factory('BackendUrl', function() {
 	}
 })
 
-app.run(function($rootScope) {
+app.run(function($rootScope, $state) {
     $rootScope.backendUrl = $backendUrl;
+    $rootScope.$on('$stateChangeError', function(event, toState, toParams, fromState, fromParams, error) {
+    	event.preventDefault();
+    	$state.get('error').error = error.data;
+    	return $state.go('error', {code: error.status}, {reload: true});
+    });
 })
 
--- app/scripts/modules/list_documents.js
+++ app/scripts/modules/list_documents.js
@@ -3,7 +3,7 @@
 
 var module = angular.module('listDocumentsModule', [])
     
-module.factory('Documents',  function($resource) {
+module.factory('Document',  function($resource) {
 	return $resource($backendUrl+'document/:action/', {}, {
 		list: {
 			method: 'GET', 
@@ -11,55 +11,169 @@ module.factory('Documents',  function($resource) {
 				action: 'list'
 			}, 
 			isArray: true
+		},
+		save: {
+			method: 'POST', 
+			params: {
+				action: 'save'
+			}
+		},
+		remove: {
+			method: 'DELETE', 
+			params: {
+				action: 'remove'
+			}
 		}
 	});
 });
 
-module.controller('listDocumentsCtrl', function ($scope, $resource, $state, $modal, Documents) {
+module.controller('listDocumentsCtrl', function ($scope, $resource, $state, $modal, Document) {
 	$scope.path = $state.params.path.split("/");
+	$scope.currentPath = "/" + $state.params.path;
+		
+	/*
+	$('.download-btn').tooltip({
+	   container: 'body',
+		title: "Descargar"
+	}) 
+	$('.remove-btn').tooltip({
+		container: 'body',
+		title: "Borrar"
+	}) 
+	*/
 	
-	Documents.list({path: "/" + $state.params.path}, function(result){
+	Document.list({path: $scope.currentPath}, function(result){
 		$scope.documents = result;
 	})
 	
-	$scope.viewDocument = function(docId) {
+	$scope.viewDocument = function(doc, editMode) {
+		var templateUrl, controller;
+		
+		if(doc == null || doc == "document") {
+			templateUrl = 'views/view_document.html'
+			controller = 'viewDocumentCtrl'
+		} else if(doc == "folder") {
+			templateUrl = 'views/view_folder.html'
+			controller = 'viewFolderCtrl'
+		} else {
+			templateUrl = doc.isFolder ? 'views/view_folder.html' : 'views/view_document.html'
+			controller = doc.isFolder ? 'viewFolderCtrl' : 'viewDocumentCtrl'
+		}
+			
 		var modalInstance = $modal.open({
-		      templateUrl: 'views/view_document.html',
-		      controller: 'viewDocumentCtrl',
+		      templateUrl: templateUrl,
+		      controller: controller,
 		      resolve: {
 		      	document: function () {
-		      		return $.grep($scope.documents, function(doc){ return doc.id == docId; })[0];
+		      		if(typeof doc != "string")
+		      			return $.extend(true, {}, doc);
+		      		else 
+		      			return {}
+		      	}, 
+		      	editMode: function() {
+		      		return editMode
+		      	},
+		      	currentPath: function() {
+		      		return $scope.currentPath
 		      	}
 		      }
 		});
-		/*
-		modalInstance.result.then(function (document) {
-		      $scope.document = document; //TODO: renovar datos de verdad
-		      var d = $.grep($scope.documents, function(doc){ return doc.id == docId; })[0]
-		      d = document;
-		    }, function () {
-		      $log.info('Modal dismissed at: ' + new Date());
-		});*/
 	}
 	
+	/*
+	$scope.viewFolder = function(folderId, editMode) {
+		var modalInstance = $modal.open({
+		      templateUrl: 'views/view_folder.html',
+		      controller: 'viewFolderCtrl',
+		      size: 'sm',
+		      resolve: {
+			    editMode: function() {
+		      		return editMode
+		      	}
+			  }
+		});		
+		
+		modalInstance.result.then(function (folderName) {
+		      var newFolder = new Document({name: folderName, path: $scope.currentPath, isFolder: true})
+		      newFolder.$save(function(response){
+		    	  $scope.documents.unshift(response)
+		      })
+		});
+	}
+	*/
+	$scope.remove = function(docId) {
+		if (confirm('¿Desea borrar el fichero?')) {
+			Document.remove({id: docId}, function(result){
+				if(result.remove == "OK") {
+					$state.go($state.current, {}, {reload: true});
+				} else {
+					alert("Ha ocurrido un error al borrar el Documento.")
+				}
+			})
+		}
+	}
 });
 
-module.controller('viewDocumentCtrl', function ($scope, $modalInstance, document) {
+module.controller('viewDocumentCtrl', function ($scope, $modalInstance, $upload, $state, Document, document, editMode, currentPath) {	
+	$scope.document = document; 
 	
-	document.name = "aaaaaa"
 	
-	//$scope.document = document;
-	
+	$scope.onFileSelect = function($files) {
+		$scope.file = $files[0]
+		if(!document.id)
+			$scope.document.name = $scope.file.name;
+		$scope.fileName = $scope.file.name;
+	}
 	
 	$scope.ok = function () {
-		$modalInstance.close($scope.document);
+		$.extend($scope.document, {path: currentPath, isFolder: false})
+	    var newDocument = new Document($scope.document)
+		$scope.uploading = true;
+		newDocument.$save(function(response){
+			if($scope.file) {
+				$scope.upload = $upload.upload({
+			        url: $backendUrl+'document/upload', 
+			        method: 'POST',
+			        data: {id: response.id},
+			        file: $scope.file,
+			    }).progress(function(evt) {
+			    	$scope.progress = parseInt(100.0 * evt.loaded / evt.total);
+			    }).success(function(data, status, headers, config) {
+			        $modalInstance.close(newDocument);
+			        $state.go($state.current, {}, {reload: true});
+			        $scope.uploading = false;
+			    }).error(function(data) {
+			    	alert("Ha ocurrido un error subiendo el fichero.")
+			    });		
+			} else{
+				$state.go($state.current, {}, {reload: true});
+				$modalInstance.close(response);
+			}
+	    })
 	};
-
 	$scope.cancel = function () {
+		if($scope.upload && $scope.uploading) {
+			$scope.upload.abort()
+		}
 		$modalInstance.dismiss('cancel');
 	};
 })
 
 
+module.controller('viewFolderCtrl', function ($scope, $modalInstance, $state, Document, document, editMode, currentPath) {
+	$scope.document = document; 
+	
+	$scope.ok = function () {
+		$.extend($scope.document, {path: currentPath, isFolder: true})
+		var newFolder = new Document($scope.document)
+		newFolder.$save(function(response){
+			$state.go($state.current, {}, {reload: true});
+			$modalInstance.close(response);
+		})
+	};
+	$scope.cancel = function () {
+		$modalInstance.dismiss('cancel');
+	};
+})
 
 
